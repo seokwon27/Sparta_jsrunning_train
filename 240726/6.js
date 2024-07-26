40c412f59e19ceaa31596897a95941490706ca50
unknown <tjrtkd95@gmail.com>
2024-07-26 17:41:10 +0900

    6번 문제 풀이

--- 240726/6.js
+++ 240726/6.js
@@ -7,10 +7,19 @@ const students = [
 ];
 
 // 여기에 코드를 작성해주세요
+arr = students
+.filter((student) => {
+  let avg =
+  student.scores.reduce((a,b)=> a + b , 0) / student.scores.length;
+  return avg >= 80;
+})
+.map((student) => student.name);
 
+console.log(arr);
 // 예상 출력: ["이재상", "김준현"]
 
 //2.
 const numbers = [5, 10, 15, 20, 25];
 // 요소에 2를 곱하고, 30 이하인 것들만 반환받아 출력해주세요
 // 출력값 : [10, 20, 30]
+console.log(numbers.map((a) => a*2).filter((a) => a <= 30));
